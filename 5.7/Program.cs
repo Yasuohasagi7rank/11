fe770bbdbf7b8cf4af36b59ba0cbf9eda92def7a
Yasuohasagi7rank <112807477+Yasuohasagi7rank@users.noreply.github.com>
2022-11-21 01:04:06 +0300

    Add files via upload

---
+++ 5.7/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _5._7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double v = 20.4 ;
+            for (int a = 1; a < 21; a++) Console.WriteLine($"{a}\t{v * a}");
+            Console.ReadKey();
+            Console.Clear();
+        }
+    }
+}
